d572cbf28f36424c7f468fffe974c96c26dff195
Gavin Yip <gavin.yip88@gmail.com>
2023-12-04 13:51:47 +0000

    supabase1

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,20 @@
-"use client";
 
-import Link from "next/link";
+
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Link href="/">Home</Link>
-        <Link href="/messages">Messages</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/register">Register</Link>
-        <Link href="/signIn">Sign In</Link>
+    <div className="row">
+      <div className="col-6">
+        <h1 className="header">Supabase Auth + Storage</h1>
+        <p className="">
+          Experience our Auth and Storage through a simple profile management example. Create a user
+          profile and upload an avatar image. Fast, simple, secure.
+        </p>
       </div>
-      <div>
-        <h1 className="bg-nesstYellow text-white p-1 text-1xl font-serif font-bold font-montserrat">
-          Hello
-        </h1>
-        <h1 className="bg-nesstLightGrey text-white p-2 text-2xl font-sans hover:underline font-montserrat">
-          Hello
-        </h1>
-        <h1 className="bg-nesstDarkGrey text-white p-3 text-3xl font-mono underline border-solid border-white border-4 text-center font-montserrat">
-          Hello
-        </h1>
+      <div className="col-6 auth-widget">
+        <AuthForm />
       </div>
-    </main>
-  );
+    </div>
+  )
 }
