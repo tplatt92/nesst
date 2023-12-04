93ef388876a83c7af1eac8b19242b76eab01268e
Gavin Yip <gavin.yip88@gmail.com>
2023-12-04 14:24:39 +0000

    profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,7 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { Database } from '@/types/supabase'
-import AccountForm from './account-form'
+import AccountForm from '../components/AccountForm'
 
 export default async function Account() {
   const supabase = createServerComponentClient<Database>({ cookies })
