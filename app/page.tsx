9a8920db1d19e9cd2771944dc4c39c6a39dbe7ec
Gavin Yip <gavin.yip88@gmail.com>
2023-12-04 16:55:11 +0000

    logo on page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className=" h-screen flex flex-col justify-center bg-black ">
       <div className="col p-16 text-gray-500">
+        <img src="/logos/emptyegg.png" className="h-20" />
+        <h1 className="header">NEST</h1>
         <AuthForm />
-        <h1 className="header">Supabase Auth + Storage</h1>
         <p className="">N e s s t</p>
       </div>
       <div className="col-6 auth-widget"></div>
