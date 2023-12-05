034f2ecb8947f853fe7dc9364e806777b4159d52
“Katie-W-22” <katiecoder22@gmail.com>
2023-12-05 11:25:35 +0000

    links to footer icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,13 +3,23 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import { UserIcon } from "@heroicons/react/24/solid";
 
+import Link from 'next/link'
+
 export default function Footer() {
   return (<> 
-  <div className="flex w-3/4 border-solid p-4">
-  <HeartIcon className="h-6 w-1/4 text-blue-500"/>
-  <MagnifyingGlassIcon className="h-6 w-1/4 text-blue-500"/>
-  <ChatBubbleLeftEllipsisIcon className="h-6 w-1/4 text-blue-500"/>
-  <UserIcon className="h-6 w-1/4 text-blue-500"/>
+  <div className="flex items-center justify-between bg-red-200">
+  <Link href="/explore"><MagnifyingGlassIcon className="h-20 w-1/4 text-blue-500"/></Link>
+
+  <Link href="/favourites"><HeartIcon className="h-20 w-1/4 text-blue-500"/></Link>
+  
+  <Link href="/messages"><ChatBubbleLeftEllipsisIcon className="h-20 w-1/4 text-blue-500"/></Link>
+  <Link href="/profile"><UserIcon className="h-20 w-1/4 text-blue-500"/></Link>
   </div>
   </>);
 }
+
+
+/*
+flex w-3/4 border-solid p-4
+
+<Link href="/messages"><ChatBubbleLeftEllipsisIcon className="h-6 w-1/4 text-blue-500"/></Link>*/
---
+++ app/favourites/page.tsx
@@ -0,0 +1,7 @@
+export default function Favourites (){
+    return(
+        <>
+        <p>This is the Favourites page</p>
+        </>
+    )
+}
