a2de02a28bbf04551ab408ab009df37015984409
Tom Platt <thomasjplatt0107@gmail.com>
2023-12-05 16:04:54 +0000

    render tests for home component started

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,10 +1,39 @@
-import { render } from "@testing-library/react";
+import { render, cleanup, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import Home from "@/app/page";
 
+//Cleanup
+afterEach(() => {
+  cleanup();
+});
+
 describe("Home", () => {
   it("renders without crashing", () => {
     render(<Home />);
+  });
+
+  it("renders a h1 element with the text content About", () => {
+    const { getByRole } = render(<Home />);
+
+    const heading = getByRole("heading", { name: /nesst/i, level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("render a button on the page with the text registere", () => {
+    const { getByRole } = render(<Home />);
+
+    const button = getByRole("button", { name: /register/i });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("render a button on the page with the text registere", () => {
+    const { getByRole } = render(<Home />);
+
+    const button = getByRole("button", { name: /log in/i });
+
+    expect(button).toBeInTheDocument();
   });
 });
