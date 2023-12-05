d1a8b44ed8ae50f5ba0d6e9b8704a4726c4b93f9
Tom Platt <thomasjplatt0107@gmail.com>
2023-12-05 21:14:34 +0000

    login and register form styling complete

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -2,27 +2,38 @@
 import { Auth } from "@supabase/auth-ui-react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/types/supabase";
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from "@supabase/auth-ui-shared";
 
 export default function AuthForm() {
   const supabase = createClientComponentClient<Database>();
-
-  const customTheme = {
-    default: {
-      colors: {
-        brand: "#d9a66d",
-        brandButtonText: "#FFF",
-      },
-    },
-  };
 
   return (
     <Auth
       supabaseClient={supabase}
       view="sign_in"
       appearance={{
-        theme: customTheme,
+        theme: ThemeSupa,
         style: {
-          container: { background: "black" },
+          button: { borderRadius: "9999px" },
+          input: { borderRadius: "9999px" },
+        },
+        variables: {
+          default: {
+            colors: {
+              brand: "#d9a66d",
+              brandAccent: "#d9a66d",
+            },
+          },
+        },
+      }}
+      localization={{
+        variables: {
+          sign_in: {
+            social_provider_text: "Sign in",
+          },
         },
       }}
       showLinks={true}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import { Auth } from "@supabase/auth-ui-react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/types/supabase";
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from "@supabase/auth-ui-shared";
 
 export default function SignUp() {
   const supabase = createClientComponentClient<Database>();
@@ -23,9 +27,25 @@ export default function SignUp() {
             supabaseClient={supabase}
             view="sign_up"
             appearance={{
-              theme: customTheme,
+              theme: ThemeSupa,
               style: {
-                container: { background: "black" },
+                button: { borderRadius: "9999px" },
+                input: { borderRadius: "9999px" },
+              },
+              variables: {
+                default: {
+                  colors: {
+                    brand: "#d9a66d",
+                    brandAccent: "#d9a66d",
+                  },
+                },
+              },
+            }}
+            localization={{
+              variables: {
+                sign_in: {
+                  social_provider_text: "Sign in",
+                },
               },
             }}
             showLinks={true}
