c9741082cf19a0e5dec297a2444d6d2145a85d2c
Gavin Yip <gavin.yip88@gmail.com>
2023-12-05 11:41:10 +0000

    gray icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,14 +7,23 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (<> 
-  <div className="flex items-center justify-between bg-red-200">
-  <Link href="/explore"><MagnifyingGlassIcon className="h-20 w-1/4 text-blue-500"/></Link>
+  <div className="flex items-center justify-between py-4 border-t border-gray-200 border-solid" >
+      <Link href="/explore" className="flex-1 text-center">
+        <MagnifyingGlassIcon className="h-12 text-gray-400 px-8" />
+      </Link>
 
-  <Link href="/favourites"><HeartIcon className="h-20 w-1/4 text-blue-500"/></Link>
-  
-  <Link href="/messages"><ChatBubbleLeftEllipsisIcon className="h-20 w-1/4 text-blue-500"/></Link>
-  <Link href="/profile"><UserIcon className="h-20 w-1/4 text-blue-500"/></Link>
-  </div>
+      <Link href="/favourites" className="flex-1 text-center">
+        <HeartIcon className="h-12 text-gray-400 px-8" />
+      </Link>
+
+      <Link href="/messages" className="flex-1 text-center">
+        <ChatBubbleLeftEllipsisIcon className="h-12 text-gray-400 px-8" />
+      </Link>
+
+      <Link href="/profile" className="flex-1 text-center">
+        <UserIcon className="h-12 text-gray-400 px-8" />
+      </Link>
+    </div>
   </>);
 }
 
@@ -22,4 +31,4 @@ export default function Footer() {
 /*
 flex w-3/4 border-solid p-4
 
-<Link href="/messages"><ChatBubbleLeftEllipsisIcon className="h-6 w-1/4 text-blue-500"/></Link>*/
+<Link href="/messages"><ChatBubbleLeftEllipsisIcon className="h-6 w-1/4 text-gray-400"/></Link>*/
