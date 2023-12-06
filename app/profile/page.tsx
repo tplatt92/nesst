b6b8c5023ec1fd5455d138f1b6ffa91a2d81c382
Tom Platt <thomasjplatt0107@gmail.com>
2023-12-06 13:41:14 +0000

    profile fetch

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,72 +1,46 @@
-// fetch profile data
-// display in fields.
-
-// import Link from "next/link";
-// import { supabase } from "../components/AuthForm";
-// import { notFound } from "next/navigation";
-
-// import Footer from "../components/Footer";
-
-// export const revalidate = 0;
-
-// export async function generateStaticParams() {
-//   const { data: profiles } = await supabase
-//     .from("profiles")
-//     .select("id, bio, username, avatarUrl, age");
-//   console.log(profiles);
-//   return profiles;
-// }
-
-// export default async function Profiles({
-//   params: { userId },
-// }: {
-//   params: { userId: string };
-// }) {
-//   const { data: profiles } = await supabase
-//     .from("profiles")
-//     .select()
-//     .match({ userId })
-//     .single();
-
-//   if (!profiles) {
-//     notFound();
-//   }
-
-//   const data = JSON.stringify(profiles, null, 2);
-
-//   return (
-//     <>
-//       <div>
-//         <h1>Profile</h1>
-//         <Link href={"/profile/edit"}>Edit</Link>
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// }
-
 "use client";
+import { useCallback, useEffect, useState } from "react";
+import { Database } from "@/types/supabase";
+import {
+  Session,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import Image from "next/image";
+import Avatar from "../components/Avatar";
 
-import { useEffect, useState } from "react";
-import { supabase } from "../utils/supabase.tsx";
+export default function Profile({ session }: { session: Session | null }) {
+  const supabase = createClientComponentClient<Database>();
+  const [loading, setLoading] = useState(true);
+  const user = session?.user;
 
-export default function ClientPosts() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [profiles, setProfiles] = useState<any>([]);
+  const getProfile = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const { data, error, status } = await supabase
+        .from("profiles")
+        .select(
+          `first_name, last_name, username, age, avatar_url, bio, drinker, smoker`
+        )
+        .eq("id", user?.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        console.log(data);
+        return data;
+      }
+    } catch (error) {
+      alert("Error loading user data!");
+    } finally {
+      setLoading(false);
+    }
+  }, [user, supabase]);
 
   useEffect(() => {
-    const fetchProfiles = async () => {
-      const { data } = await supabase.from("profiles").select();
-      setProfiles(data);
-      setIsLoading(false);
-    };
-
-    fetchProfiles();
-  }, []);
-
-  return isLoading ? (
-    <p>Loading</p>
-  ) : (
-    <pre>{JSON.stringify(profiles, null, 2)}</pre>
-  );
+    getProfile();
+  }, [user, getProfile]);
 }
