6d07396b4ab7782b240c1969e1d9dbad9305f6e6
“Katie-W-22” <katiecoder22@gmail.com>
2023-12-07 13:43:44 +0000

    property icons and styling started

--- app/propertyInfoPage/page.tsx
+++ app/propertyInfoPage/page.tsx
@@ -17,59 +17,121 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+// ... (imports remain unchanged)
+
 export default function Explore() {
-  const [properties, setProperties] = useState<null | any[]>(null);
-  const [fetchError, setFetchError] = useState<string | null>(
-    "error fetching properties"
-  );
-
-  useEffect(() => {
-    const fetchProperties = async () => {
-      try {
-        const { data, error } = await supabase.from("properties").select("*");
-
-        if (error) {
-          setFetchError("error fetching properties");
-          setProperties(null);
-          console.error(error);
+    const [properties, setProperties] = useState<null | any[]>(null);
+    const [fetchError, setFetchError] = useState<string | null>(
+      "error fetching properties"
+    );
+  
+    useEffect(() => {
+      const fetchProperties = async () => {
+        try {
+          const { data, error } = await supabase.from("properties").select("*");
+  
+          if (error) {
+            setFetchError("error fetching properties");
+            setProperties(null);
+            console.error(error);
+          }
+          if (data) {
+            setProperties(data);
+            setFetchError(null);
+          }
+        } catch (error) {
+          console.error("An unexpected error occurred:", error);
         }
-        if (data) {
-          setProperties(data);
-          setFetchError(null);
-        }
-      } catch (error) {
-        console.error("An unexpected error occurred:", error);
-      }
-    };
-    fetchProperties();
-  }, []);
+      };
+      fetchProperties();
+    }, []);
+  
+    //number of clicked property (needs to be linked to the property clicked somehow)
+    let propertyNumberClicked = 2
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10 pb-20">
-      
-      <div className="grid grid-col-1 gap-4" data-testid="card-id">
-        {properties?.map((properties) => (
-          <Card key={properties.id}>
-            <CardHeader className="relative">
-              <Carousel images={properties.image} />
-            </CardHeader>
-            <CardContent>
-              <CardTitle className="text-xl font-monserrat font-semibold">
-                {properties.name}
-              </CardTitle>
-              <CardDescription className="text-yellow-600 text-base font-medium">
-                {properties.location}
-              </CardDescription>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-            <p className="font-medium">{properties.description}</p>
-              <p className="font-medium">{properties.price}</p>
-              <p className="text-gray-400 ">{properties.date}</p>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
-      <Footer />
-    </main>
-  );
-}
+    // Display only the clicked property
+    const clickedProperty = properties?.[propertyNumberClicked];
+  
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-10 pb-20">
+        <div className="grid grid-col-1 gap-4" data-testid="card-id">
+          {clickedProperty && (
+            <Card key={clickedProperty.id}>
+              <CardHeader className="relative">
+                <Carousel images={clickedProperty.image} />
+              </CardHeader>
+              <CardContent>
+                <CardTitle className="text-xl font-monserrat font-semibold">
+                  {clickedProperty.name}
+                </CardTitle>
+                <CardDescription className="text-yellow-600 text-base font-medium">
+  <span style={{ display: 'flex', alignItems: 'center' }}>
+    <span dangerouslySetInnerHTML={{ __html: '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M32 32c17.7 0 32 14.3 32 32V320H288V160c0-17.7 14.3-32 32-32H544c53 0 96 43 96 96V448c0 17.7-14.3 32-32 32s-32-14.3-32-32V416H352 320 64v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V64C0 46.3 14.3 32 32 32zm144 96a80 80 0 1 1 0 160 80 80 0 1 1 0-160z"/></svg>' }} />
+    <span style={{ marginLeft: '4px' }}>{clickedProperty.beds}</span>
+  </span>
+</CardDescription>
+              </CardContent>
+              <CardFooter className="flex flex-col justify-between" style={{ alignItems: 'flex-start' }}>
+                <p className="font-medium">{clickedProperty.description}</p>
+                <p className="font-medium">£{clickedProperty.price}/month</p>
+                <p className="text-gray-400 ">{clickedProperty.date}</p>
+                <p className="font-medium">
+  {clickedProperty.wifi ? (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
+      />
+    </svg>
+  ) : (
+    'NO WIFI'
+  )}
+</p>
+
+                
+                <p className="text-gray-400">
+  <span style={{ display: 'flex', alignItems: 'center' }}>
+    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512" style={{ marginRight: '4px' }}>
+      <path d="M96 77.3c0-7.3 5.9-13.3 13.3-13.3c3.5 0 6.9 1.4 9.4 3.9l14.9 14.9C130 91.8 128 101.7 128 112c0 19.9 7.2 38 19.2 52c-5.3 9.2-4 21.1 3.8 29c9.4 9.4 24.6 9.4 33.9 0L289 89c9.4-9.4 9.4-24.6 0-33.9c-7.9-7.9-19.8-9.1-29-3.8C246 39.2 227.9 32 208 32c-10.3 0-20.2 2-29.2 5.5L163.9 22.6C149.4 8.1 129.7 0 109.3 0C66.6 0 32 34.6 32 77.3V256c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H96V77.3zM32 352v16c0 28.4 12.4 54 32 71.6V480c0 17.7 14.3 32 32 32s32-14.3 32-32V464H384v16c0 17.7 14.3 32 32 32s32-14.3 32-32V439.6c19.6-17.6 32-43.1 32-71.6V352H32z"/>
+    </svg>
+    {clickedProperty.bathrooms}
+  </span>
+</p>
+<p className="text-gray-400 "> Area ={clickedProperty.area} m<sup>2</sup></p>
+                <p className="font-medium">
+                  {clickedProperty.availability ? 'AVAIL' : 'NOT AVAIL'}
+                </p>
+              </CardFooter>
+            </Card>
+          )}
+          
+        </div>
+        <Footer />
+      </main>
+    );
+  }
+  
+
+  //wifi svg
+  /* 
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+</svg>
+
+//bed svg
+<svg xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M32 32c17.7 0 32 14.3 32 32V320H288V160c0-17.7 14.3-32 32-32H544c53 0 96 43 96 96V448c0 17.7-14.3 32-32 32s-32-14.3-32-32V416H352 320 64v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V64C0 46.3 14.3 32 32 32zm144 96a80 80 0 1 1 0 160 80 80 0 1 1 0-160z"/></svg>
+
+//bath svg
+<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M96 77.3c0-7.3 5.9-13.3 13.3-13.3c3.5 0 6.9 1.4 9.4 3.9l14.9 14.9C130 91.8 128 101.7 128 112c0 19.9 7.2 38 19.2 52c-5.3 9.2-4 21.1 3.8 29c9.4 9.4 24.6 9.4 33.9 0L289 89c9.4-9.4 9.4-24.6 0-33.9c-7.9-7.9-19.8-9.1-29-3.8C246 39.2 227.9 32 208 32c-10.3 0-20.2 2-29.2 5.5L163.9 22.6C149.4 8.1 129.7 0 109.3 0C66.6 0 32 34.6 32 77.3V256c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H96V77.3zM32 352v16c0 28.4 12.4 54 32 71.6V480c0 17.7 14.3 32 32 32s32-14.3 32-32V464H384v16c0 17.7 14.3 32 32 32s32-14.3 32-32V439.6c19.6-17.6 32-43.1 32-71.6V352H32z"/></svg>
+
+//house svg
+<svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
+  */
