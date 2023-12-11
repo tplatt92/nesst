fa0adae1aba728cb03175a24aa1491ec8f5830e6
Gavin Yip <gavin.yip88@gmail.com>
2023-12-11 14:48:22 +0000

    seach destination case insens

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -5,7 +5,8 @@ import supabase from "../config/SuperbaseClient";
 type SearchProps = {
   setProperties: React.Dispatch<React.SetStateAction<null | any[]>>;
 };
-const Search:React.FC<SearchProps>=({setProperties}) => {
+type FormSubmit = React.FormEvent<HTMLFormElement>;
+const Search: React.FC<SearchProps> = ({ setProperties }) => {
   // const [properties, setProperties] = useState<null | any[]>(null);
   const [fetchError, setFetchError] = useState<string | null>(
     "error fetching properties"
@@ -16,13 +17,14 @@ const Search:React.FC<SearchProps>=({setProperties}) => {
 
   const [availability, setAvailability] = useState<null | boolean>(null);
 
-  useEffect(() => {
+  function handleSubmit(e: FormSubmit) {
+    e.preventDefault();
     const fetchProperties = async () => {
       try {
         const { data, error } = await supabase
           .from("properties")
           .select("*")
-          .eq("location", location);
+          .ilike("location", location);
 
         if (error) {
           setFetchError("error fetching properties");
@@ -38,10 +40,13 @@ const Search:React.FC<SearchProps>=({setProperties}) => {
       }
     };
     fetchProperties();
-  }, []);
+  }
   return (
     <nav className="flex flex-row relative my-4 w-full">
-      <form className="flex items-center w-full  justify-between">
+      <form
+        className="flex items-center w-full  justify-between"
+        onSubmit={handleSubmit}
+      >
         <div className="flex flex-1 bg-white px-2 border-solid border-2 border-gray-300 rounded-full left-0 h-14 lg:h-8 items-center lg:max-w-xs">
           <input
             className="h-12 lg:h-6 items-center flex-1 rounded-l-full lg:text-xs "
@@ -50,7 +55,7 @@ const Search:React.FC<SearchProps>=({setProperties}) => {
             value={location}
             onChange={(e) => setLocation(e.target.value)}
           />
-          <button type="button">
+          <button type="submit">
             <MagnifyingGlassIcon className="h-10 lg:h-6 text-white ml-3 bg-nesstYellow p-1 rounded-full" />
           </button>
         </div>
@@ -60,6 +65,6 @@ const Search:React.FC<SearchProps>=({setProperties}) => {
       </form>
     </nav>
   );
-}
+};
 
 export default Search;
