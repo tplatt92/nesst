c91012048476caa586573ef8845074f6ebe75fd5
Gavin Yip <gavin.yip88@gmail.com>
2023-12-12 17:01:33 +0000

    attempt 1

--- app/components/ExploreNav.tsx
+++ app/components/ExploreNav.tsx
@@ -1,16 +1,21 @@
 import Search from "./Search";
+import Filter from "./Filter";
 type ExploreNavProps = {
   setProperties: React.Dispatch<React.SetStateAction<null | any[]>>;
+  properties: any[] | null;
 };
 type FilterProps = {
   setProperties: React.Dispatch<React.SetStateAction<null | number>>;
 };
-const ExploreNav: React.FC<ExploreNavProps> = ({ setProperties }) => {
+const ExploreNav: React.FC<ExploreNavProps> = ({
+  setProperties,
+  properties,
+}) => {
   return (
     <nav className="flex flex-row items-center gap-4 relative my-4 w-full">
       <Search setProperties={setProperties} />
+      <Filter properties={properties} setProperties={setProperties} />
     </nav>
   );
 };
 export default ExploreNav;
-
--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -46,7 +46,7 @@ export default function Explore() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-4 lg:px-8 pb-32">
-      <ExploreNav setProperties={setProperties} />
+      <ExploreNav setProperties={setProperties} properties={properties}/>
       <div
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4"
         data-testid="card-id"
