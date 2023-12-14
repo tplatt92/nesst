aefcb705401dcf6ebf1ce0bde4254266f14fc0d3
yusra-aina <yusrachoudhury123@gmail.com>
2023-12-14 16:50:28 +0000

    responsive designs

--- app/components/ExploreNav.tsx
+++ app/components/ExploreNav.tsx
@@ -4,10 +4,6 @@ import Search from "./Search";
 import FilterSheet from "./FilterSheet";
 import { useMediaQuery } from "react-responsive";
 import Link from "next/link";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { HeartIcon } from "@heroicons/react/24/solid";
-import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/24/solid";
-import { UserIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
 type ExploreNavProps = {
@@ -28,37 +24,48 @@ const ExploreNav: React.FC<ExploreNavProps> = ({ setProperties }) => {
         </>
       ) : (
         <div className="flex flex-row items-center gap-4 relative my-4 w-full">
-  <div className="flex items-center justify-center pl-2">
-          <Image
-          
-            alt="NESST"
-            src="/logos/fullegg.png" // Use forward slashes instead of backslashe
-            priority={true}
-            width={100}
-            height={100}
-          />
-         {/* <p className="text-xs text-gray-400">My Nesst</p> */}
-        </div>
-        
+          <div className="flex items-center justify-center pl-2">
+            <Image
+              alt="NESST"
+              src="/logos/fullegg.png" // Use forward slashes instead of backslashe
+              priority={true}
+              width={100}
+              height={100}
+            />
+            {/* <p className="text-xs text-gray-400">My Nesst</p> */}
+          </div>
+
           <Search setProperties={setProperties} />
 
           <div>
-            <Link href="/explore" className="flex-1 text-center">
+            <Link
+              href="/explore"
+              className="flex-1 text-center hover:underline  hover:text-nesstYellow cursor-pointer"
+            >
               <p className=" text-md">Explore</p>
             </Link>
           </div>
           <div>
-            <Link href="/favourites" className="flex-1 text-center">
+            <Link
+              href="/favourites"
+              className="flex-1 text-center hover:underline  hover:text-nesstYellow cursor-pointer"
+            >
               <p className=" text-md">Favourites</p>
             </Link>
           </div>
           <div>
-            <Link href="/messages" className="flex-1 text-center">
+            <Link
+              href="/messages"
+              className="flex-1 text-center hover:underline  hover:text-nesstYellow cursor-pointer"
+            >
               <p className=" text-md">Messages</p>
             </Link>
           </div>
           <div>
-            <Link href="/profile" className="flex-1 text-center">
+            <Link
+              href="/profile"
+              className="flex-1 text-center hover:underline  hover:text-nesstYellow cursor-pointer"
+            >
               <p className=" text-md">Profile</p>
             </Link>
           </div>
