083493f5b7dbcd41cc94225e58b2e43424a86839
Gavin Yip <gavin.yip88@gmail.com>
2023-12-18 17:07:15 +0000

    fixed blurry pictures

--- app/components/LikedPropertiesItem.tsx
+++ app/components/LikedPropertiesItem.tsx
@@ -24,10 +24,10 @@ const LikedPropertiesItem: React.FC<PropertyData> = ({
       <div>
         <Image
           src={`${url}`}
-          width={80}
-          height={80}
+          width={240}
+          height={240}
           alt="property image"
-          className="lg:w-[240px]"
+          className="lg:w-[240px] lg:h-[140px] sm:w-[80px] w-[120px] h-[80px] rounded-xl"
         />
       </div>
       <div className="border-y flex-1 py-8 lg:py-14">
