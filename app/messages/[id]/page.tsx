6e0b0aa922a2cf700bbfed05d5ce5eb396d53999
Gavin Yip <gavin.yip88@gmail.com>
2023-12-20 12:03:11 +0000

    scroll to bottom and right sided bubbles not working

--- app/messages/[id]/page.tsx
+++ app/messages/[id]/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/types/supabase";
 import Messages from "../../components/Messages";
+import { ProfileCache } from "@/types/types";
 export default function Chat() {
   const supabase = createClientComponentClient<Database>();
 
@@ -29,15 +30,22 @@ export default function Chat() {
         <p>Avatar</p>
         <h1>Name</h1>
       </header>
-      <main className="flex flex-1 justify-between flex-col">
+      <main className="flex flex-1 justify-between flex-col max-h-[89%]">
         <div className="overflow-y-scroll">
-          <p>content</p>
-          <Messages />
+          <Messages
+            roomId={""}
+            profileCache={{}}
+            setProfileCache={function (
+              value: React.SetStateAction<ProfileCache>
+            ): void {
+              throw new Error("Function not implemented.");
+            }}
+          />
         </div>
         <div className="p-4">
           <form onSubmit={handleSubmit}>
             <input
-              className="bg-gray-100 py-2 w-full"
+              className="bg-gray-100 py-2 w-full fixed bottom-0 left-0"
               type="message"
               name="message"
             ></input>
