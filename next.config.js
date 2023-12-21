62e7edbe5bf7d5d9d68f470b395b52f9189538e7
Tom Platt <thomasjplatt0107@gmail.com>
2023-12-21 16:15:38 +0000

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,25 +1,16 @@
 module.exports = {
   images: {
-    domains: ['images.unsplash.com'], // Add any other domains if necessary 
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.pexels.com",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
     ],
   },
 };
-
-module.exports = {
-    images: {
-      domains: ['images.unsplash.com'], // Add any other domains if necessary 
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.pexels.com",
-          pathname: "**",
-        },
-      ],
-    },
-  };
