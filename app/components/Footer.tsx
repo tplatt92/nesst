f6e235b5d7a4d3872fdfdaa9b42c672251604e12
Tom Platt <thomasjplatt0107@gmail.com>
2023-12-21 16:12:08 +0000

    changed footer styles

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -11,74 +11,69 @@ import { usePathname } from "next/navigation";
 const Footer: React.FC<FooterProps> = ({ pathnameUrl }) => {
   return (
     <>
-      
+      <div
+        className="flex items-center justify-evenly py-4 border-t fixed border-gray-200 border-solid bottom-0 w-full bg-white md:hidden"
+        data-testid="footer-id"
+      >
         <div
-          className="flex items-center justify-evenly py-4 border-t fixed border-gray-200 border-solid bottom-0 w-full bg-white md:hidden"
-          data-testid="footer-id"
+          className={`${
+            pathnameUrl === "/explore" ? "text-nesstDarkGrey" : "text-gray-400"
+          } `}
         >
-          <div
-            className={`${
-              pathnameUrl === "/explore"
-                ? "text-nesstDarkGrey"
-                : "text-gray-400"
-            } `}
-          >
-            <Link href="/explore" className="flex-1 text-center">
-              <MagnifyingGlassIcon className="h-8  px-4" />
-              <p className=" text-xs">EXPLORE</p>
-            </Link>
-          </div>
-          <div
-            className={`${
-              pathnameUrl === "/favourites"
-                ? "text-nesstDarkGrey"
-                : "text-gray-400"
-            } `}
-          >
-            <Link href="/favourites" className="flex-1 text-center">
-              <HeartIcon className="h-8  px-4" />
-              <p className=" text-xs">FAVOURITES</p>
-            </Link>
-          </div>
-          <div className="relative self-end">
-            <Image
-              alt="NESST"
-              src="/logos/fullegg.png"
-              className="flex-1 text-center absolute bottom-5  "
-              priority={true}
-              width={60}
-              height={60}
-              style={{ width: "auto", height: "auto" }}
-            />
-            <p className="text-sm text-nesstDarkGrey">NESST</p>
-          </div>
-
-          <div
-            className={`${
-              pathnameUrl === "/messages"
-                ? "text-nesstDarkGrey"
-                : "text-gray-400"
-            } `}
-          >
-            <Link href="/messages" className="flex-1 text-center">
-              <ChatBubbleLeftEllipsisIcon className="h-8  px-4" />
-              <p className=" text-xs ">MESSAGES</p>
-            </Link>
-          </div>
-          <div
-            className={`${
-              pathnameUrl === "/profile"
-                ? "text-nesstDarkGrey"
-                : "text-gray-400"
-            } `}
-          >
-            <Link href="/profile" className="flex-1 text-center">
-              <UserIcon className="h-8  px-4" />
-              <p className=" text-xs">PROFILE</p>
-            </Link>
-          </div>
+          <Link href="/explore" className="flex-1 text-center">
+            <MagnifyingGlassIcon className="h-8  px-4" />
+            <p className=" text-xs">EXPLORE</p>
+          </Link>
         </div>
-      
+        <div
+          className={`${
+            pathnameUrl === "/favourites"
+              ? "text-nesstDarkGrey"
+              : "text-gray-400"
+          } `}
+        >
+          <Link
+            href="/favourites"
+            className="flex-1 flex flex-col items-center justify-center text-center"
+          >
+            <HeartIcon className="h-8  px-4" />
+            <p className=" text-xs">FAVOURITES</p>
+          </Link>
+        </div>
+        <div className="relative self-end">
+          <Image
+            alt="NESST"
+            src="/logos/fullegg.png"
+            className="flex-1 text-center absolute bottom-5  "
+            priority={true}
+            width={60}
+            height={60}
+            style={{ width: "auto", height: "auto" }}
+          />
+          <p className="text-sm text-nesstLightGrey">NESST</p>
+        </div>
+
+        <div
+          className={`${
+            pathnameUrl === "/messages" ? "text-nesstDarkGrey" : "text-gray-400"
+          } `}
+        >
+          <Link href="/messages" className="flex-1 text-center">
+            <ChatBubbleLeftEllipsisIcon className="h-8  px-4" />
+            <p className=" text-xs ">MESSAGES</p>
+          </Link>
+        </div>
+        <div
+          className={`${
+            pathnameUrl === "/profile" ? "text-nesstDarkGrey" : "text-gray-400"
+          } `}
+        >
+          <Link href="/profile" className="flex-1 text-center">
+            <UserIcon className="h-8  px-4" />
+            <p className=" text-xs">PROFILE</p>
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
