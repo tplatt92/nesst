3f9fa27e0cfd3344a96d8a2dd3f1f2b98dcafb15
RobearP <parobert1990@gmail.com>
2023-12-24 12:29:19 +0000

    login attempt 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import { createServerComponentClient } from "@supabase//auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { Database } from "@/types/supabase";
 
 export default function Home() {
 
-  const supabase = createServerComponentClient<Database>({ cookies });
-
-
-  supabase.auth.onAuthStateChange(async (event) => {
-    if (event == "SIGNED_IN") {
+    if ({cookies}) {
       window.location.href = "/explore";
     }
-  });
+
 
   return (
     <div className=" h-screen w-screen flex flex-col lg:flex-row items-center lg:justify-between justify-center text-white overflow-hidden bg-[url('/backgroundImages/home2.jpg')] lg:bg-none lg:bg-black bg-cover">
