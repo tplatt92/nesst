05aaf84e82b669e1afe9e7c8c2b579a6cc97241b
Gavin Yip <gavin.yip88@gmail.com>
2023-12-27 17:27:09 +0000

    apply btton failing

---
+++ __tests__/filterSheet.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import FilterSheet from "../app/components/FilterSheet";
+
+afterEach(() => {
+  cleanup();
+});
+
+describe("FilterSheet", () => {
+  it("renders without crashing", () => {
+    render(<FilterSheet />);
+  });
+
+  it("render a button on the page with the text ", () => {
+    const { getByRole, debug } = render(<FilterSheet />);
+    debug();
+
+    const button = getByRole("button", { name: /apply/i });
+
+    expect(button).toBeInTheDocument();
+  });
+});
