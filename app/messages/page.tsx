a4a5472ca8d67c2a8057b7781274523208937546
Gavin Yip <gavin.yip88@gmail.com>
2024-01-05 11:37:43 +0000

    padding for messages page

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -137,7 +137,7 @@ const Messages: React.FC<ProfileIdProps> = () => {
       <header className="hidden md:block p-0">
         <DesktopNav />
       </header>
-      <div className="flex flex-col items-center w-screen overflow-y-auto">
+      <div className="flex flex-col items-center w-screen overflow-y-auto pb-16">
         <main className="px-4 pt-4 max-w-5xl w-full ">
           <h1 className=" text-center text-2xl font-bold py-4 md:py-8">
             Messages
